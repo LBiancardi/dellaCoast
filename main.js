5cbda38a454f8087911a6255530a23a5cfcac797
Luis <luisfbiancardi@gmail.com>
2021-11-10 08:26:25 -0800

    Create list and fix bug on learn more function

--- main.js
+++ main.js
@@ -1,5 +1,21 @@
-function show_info() {
-  const itens__description = document.querySelector("#itens__description");
-
-  itens__description.classList.toggle("active");
+function func1() {
+  const b1 = document.querySelector("#b1");
+  b1.classList.toggle("active");
 }
+
+function func2() {
+  const b2 = document.querySelector("#b2");
+  b2.classList.toggle("active");
+}
+
+function func3() {
+  const b3 = document.querySelector("#b3");
+  b3.classList.toggle("active");
+}
+
+function func4() {
+  const b4 = document.querySelector("#b4");
+  b4.classList.toggle("active");
+}
+
+function show_info() {}
