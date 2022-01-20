9b94872bee6de76314fa175e6cae48a5460284bd
Luis <luisfbiancardi@gmail.com>
2022-01-20 09:13:54 -0800

    Read more animation

--- main.js
+++ main.js
@@ -1,6 +1,18 @@
-function show_info(card_position) {
-  const show_info = document.querySelectorAll(".itens");
-  // const itens_link = document.querySelectorAll(".itens__link");
+const btn = document.querySelector("read-more-btn");
+const text = document.querySelector("card__read-more");
+const cardHolder = document.querySelector(".card-holder");
 
-  show_info[card_position].classList.toggle("active");
-}
+cardHolder.addEventListener("click", (e) => {
+  console.log('i was clicked');
+  const current = e.target;
+  const isReadMoreBtn = current.className.includes("read-more-btn");
+  if (!isReadMoreBtn) return;
+
+  const currentText = e.target.parentNode.querySelector(".card__read-more");
+
+  currentText.classList.toggle("card__read-more--open");
+
+  current.textContent = current.textContent.includes("Read More...")
+    ? "Read Less..."
+    : "Read More...";
+});
